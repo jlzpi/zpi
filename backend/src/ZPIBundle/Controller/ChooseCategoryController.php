4a1a0ba577c3e5ab563872785cad09e702b5eb10
Irena Szukala <203385@student.pwr.edu.pl>
2015-03-23 11:10:39 +0100

    Dodano kontroler

---
+++ backend/src/ZPIBundle/Controller/ChooseCategoryController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ZPIBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+use JMS\Serializer\Annotation as JMS;
+use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+
+use ZPIBundle\Entity\Category;
+use ZPIBundle\Entity\CategoryRepository;
+
+class ChooseCategoryController extends FOSRestController {
+
+	/**
+	 *  Wyświetl kategorie
+	 *  @Rest\Get("/getCategoriesToDisplay")
+	 *
+	 *  @ApiDoc(
+	 *		section="wybieranie kategorii"
+	 *  )
+	 */
+	public function getCategoriesToDisplayAction(Request $request) {
+		$em = $this->getDoctrine()->getManager();
+
+		$categories = $em->getRepository('ZPIBundle:Category')->findAllCategories();
+		
+		if (count($categories) == 0) {
+			return new JsonResponse(array(
+				'FindNotNull' => false
+			));
+		}
+		else {			
+			for($i=0;$i<count($categories);$i++){
+				$tablica[$categories[$i]->getId()] = $categories[$i]->getName();
+			}
+			return new JsonResponse(array(
+				'FindNotNull' => true,			
+				'Categories' => $tablica
+			));
+		}
+	}
+}
