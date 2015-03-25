c94952ef1c02c239be813b2065fc512fe488ec25
Pawel Kotas <203439@student.pwr.edu.pl>
2015-03-25 13:38:10 +0100

    poprawki na szybciorze

--- frontend/js/showCategories.js
+++ frontend/js/showCategories.js
@@ -1,32 +1,29 @@
-﻿var tablica;
-
-$.ajax({
+﻿$.ajax({
 	type: 'GET',
 	url: ApiUrl + 'getCategoriesToDisplay',
-	dataType: 'json',
-	success: function(json) {
+	dataType: 'json'
+}).done(function(json) {
 		if (json['FindNotNull']) {
-			tablica = json['Categories'];
+			var tablica = json['Categories'];
 		}
 		else {
 			alert('Nie znaleziono kategorii.');
 		}
-	},
-	error: function(a,b, message) {
+	
+	$(document).ready(function() {
+		var x = document.getElementById('list');
+		for (i in tablica) { 
+			var option = document.createElement("option");
+			option.text = tablica[i];
+			option.name = i;
+			x.add(option);
+		}
+		$('#list').css('display', 'inline');
+		$('#choose').css('display', 'inline');
+	});
+	
+}).fail(function(a,b, message) {
 		if(message == 'Forbidden') alert('Nie jestes zalogowany jako uczen');
 		else alert('Nieznany blad');
-	}
 });
 	
-$(document).ready(function chooseCat() {
-	var x = document.getElementById('list');
-	for (i in tablica) { 
-		var option = document.createElement("option");
-		option.text = tablica[i];
-		option.name = i;
-		x.add(option);
-	}
-	$('#list').css('display', 'inline');
-	$('#choose').css('display', 'inline');
-	
-});
--- frontend/js/showPicture.js
+++ frontend/js/showPicture.js
@@ -1,26 +1,23 @@
-﻿var question;
-
-$.ajax({
+﻿$.ajax({
 	type: 'GET',
 	url: ApiUrl+'getQuestionFromCategoryToDisplay/' + getGET('category'),
-	dataType: 'json',
-	success: function(json) {
+	dataType: 'json'
+}).done(function(json) {
 		if (json['FindNotNull']) {
-			question = json;
+			var question = json;
 		}
 		else {
-			alert('Nie znaleziono obrazka.');
+			alert('Nie znaleziono kategorii.');
 		}
-	},
-	error: function(a,b, message) {
+
+	$(document).ready(function() {
+		$('#category').html('Category: ' + question.CategoryName);
+		$('#question').html(question.Question);
+		$('#picture').attr('src', PictureUrl+question.PictureDir);
+		$('#picture').css('display', 'block');
+	});
+	
+}).fail(function(a,b,message) {
 		if(message == 'Forbidden') alert('Nie jestes zalogowany jako uczen');
 		else alert('Nieznany blad');
-	}
-});
-
-$(document).ready(function() {
-	$('#category').html('Category: ' + question.CategoryName);
-	$('#question').html(question.Question);
-	$('#picture').attr('src', PictureUrl+question.PictureDir);
-	$('#picture').css('display', 'block');
 });
